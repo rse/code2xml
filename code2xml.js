00f895d9e65a45e79c881ddaaf855154e9d7d5f4
Ralf S. Engelschall <rse@engelschall.com>
2021-01-09 18:06:59 +0100

    bump year in all copyright messages

--- code2xml.js
+++ code2xml.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  code2xml -- Convert source code to syntax-annotated XML
-**  Copyright (c) 2016-2020 Dr. Ralf S. Engelschall (http://engelschall.com/)
+**  Copyright (c) 2016-2021 Dr. Ralf S. Engelschall (http://engelschall.com/)
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
